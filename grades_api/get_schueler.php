01676fc9aaa96190c7e0fd67ea7f3934f398c863
Leopold Pöchinger <poechinger@gmail.com>
2018-07-15 19:40:37 +0200

    API improved

--- grades_api/get_schueler.php
+++ grades_api/get_schueler.php
@@ -2,14 +2,22 @@
 define('WP_USE_THEMES', false);
 require_once($_SERVER['DOCUMENT_ROOT'] . "/wp-load.php");
 $sum = $wpdb->get_var( $wpdb->prepare( "SELECT schueler_settings_value FROM schueler_settings WHERE schueler_settings_name = 'api_key'") );
+if ( $_GET['apikey'] == $sum ) {
+	if ( $_GET['type'] == '1' ) {
 
-if ($_GET['apikey'] == $sum)
-{
+		$query = "SELECT * FROM schueler";
 
-	$query = "SELECT * FROM schueler";
+		$result = $wpdb->get_results( $query );
 
-	$result = $wpdb->get_results($query);
+		echo json_encode( $result );
 
-	echo json_encode($result);
+	} elseif ( $_GET['type'] == 'mitarbeit' ) {
+
+		$query = "SELECT mit.*, faecher.name FROM schueler_mitarbeit AS mit LEFT OUTER JOIN faecher ON mit.schueler_mitarbeitcol_fach=faecher.id WHERE schueler_mitarbeitcol_schueler = " . $_GET ['schueler'];
+
+		$result = $wpdb->get_results( $query );
+
+		echo json_encode( $result );
+	}
 }
 ?>
